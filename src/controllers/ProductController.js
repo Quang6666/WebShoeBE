33cf0db8b6fde96d87033861f64b99868df96080
Minh Quang <quangtrantroi@gmail.com>
2024-04-16 08:53:35 +0700

    Bao cao

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -72,7 +72,7 @@ const deleteProduct = async (req, res) => {
 const getAllProduct = async (req, res) => {
     try {
         const {limit, page } = req.query
-        const response = await ProductService.getAllProduct(Number(limit) , Number(page))
+        const response = await ProductService.getAllProduct(Number(limit) || 8 , Number(page)|| 0)
         return res.status(200).json(response)
     } catch (e) {
         return res.status(404).json({
